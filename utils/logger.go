e6f1120c6a8fcc7e55118ded7cd0bbb7e36e92ff
Terminal-Adawe <bede.abbe91@gmail.com>
2026-03-08 17:37:32 +0000

    changes

--- utils/logger.go
+++ utils/logger.go
@@ -1,16 +1,19 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/beego/beego/v2/core/logs"
 )
 
 func Logger(fileName string, lineNumber int, requestId string, logLevel string, log string) {
+	lineNumberStr := fmt.Sprintf("%d", lineNumber)
 	switch logLevel {
 	case "debug":
-		logs.Debug("File: %s:%s - %s: %s", fileName, lineNumber, requestId, log)
+		logs.Debug("File: %s:%s - %s: %s", fileName, lineNumberStr, requestId, log)
 	case "error":
-		logs.Error("File: %s:%s - %s: %s", fileName, lineNumber, requestId, log)
+		logs.Error("File: %s:%s - %s: %s", fileName, lineNumberStr, requestId, log)
 	default:
-		logs.Info("File: %s:%s - %s: %s", fileName, lineNumber, requestId, log)
+		logs.Info("File: %s:%s - %s: %s", fileName, lineNumberStr, requestId, log)
 	}
 }
