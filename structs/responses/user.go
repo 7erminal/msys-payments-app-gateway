0a58149ae62666127b9978d2d47e8e0d310f4fe7
Terminal-Adawe <bede.abbe91@gmail.com>
2026-03-23 22:22:44 +0000

    changes

--- structs/responses/user.go
+++ structs/responses/user.go
@@ -576,21 +576,21 @@ type UsersOri struct {
 }
 
 type Users struct {
-	UserId    int64 `orm:"auto"`
-	UserType  int
-	ImagePath string
-	// Customer      *UserExtraDetails `orm:"rel(fk);column(customer_id)"`
-	FullName    string    `orm:"size(255)"`
-	Username    string    `orm:"size(255)"`
-	Password    string    `orm:"size(255)"`
-	Email       string    `orm:"size(255)"`
-	PhoneNumber string    `orm:"size(255)"`
-	Gender      string    `orm:"size(10)"`
-	Dob         time.Time `orm:"type(datetime)"`
-	Address     string    `orm:"size(255)"`
-	IdType      string    `orm:"size(5)"`
-	IdNumber    string    `orm:"size(100)"`
-	// Role          *Role
+	UserId        int64 `orm:"auto"`
+	UserType      int
+	ImagePath     string
+	Customer      *UserExtraDetails `orm:"rel(fk);column(customer_id)"`
+	FullName      string            `orm:"size(255)"`
+	Username      string            `orm:"size(255)"`
+	Password      string            `orm:"size(255)"`
+	Email         string            `orm:"size(255)"`
+	PhoneNumber   string            `orm:"size(255)"`
+	Gender        string            `orm:"size(10)"`
+	Dob           time.Time         `orm:"type(datetime)"`
+	Address       string            `orm:"size(255)"`
+	IdType        string            `orm:"size(5)"`
+	IdNumber      string            `orm:"size(100)"`
+	Role          *Role
 	MaritalStatus string `orm:"size(255);omitempty"`
 	Active        int
 	IsVerified    bool
