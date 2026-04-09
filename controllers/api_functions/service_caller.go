79823774e2570aa3fa42241762468680f9c35a3f
Terminal-Adawe <bede.abbe91@gmail.com>
2026-04-09 04:51:56 +0000

    loan check

--- controllers/api_functions/service_caller.go
+++ controllers/api_functions/service_caller.go
@@ -3073,7 +3073,7 @@ func ListAccountLoans(c *beego.Controller, clientId string, accountNumber string
 	request := api.NewRequest(
 		host,
 		"/v2/api/list-account-loans/",
-		api.POST)
+		api.GET)
 	// request.Params["username"] = username
 	// request.Params = {"UserId": strconv.Itoa(int(userid))}
 	request.HeaderField["clientId"] = clientId
@@ -3081,7 +3081,7 @@ func ListAccountLoans(c *beego.Controller, clientId string, accountNumber string
 
 	client := api.Client{
 		Request: request,
-		Type_:   "body",
+		Type_:   "params",
 	}
 	res, err := client.SendRequest()
 	if err != nil {
